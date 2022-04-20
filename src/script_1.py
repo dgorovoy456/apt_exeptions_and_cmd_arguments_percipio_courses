9a016c9e4e1d117b6a5a15f50c1c08107494ba27
denys.horovyi <denys.horovyi@globallogic.com>
2022-04-20 13:30:36 +0200

    Lesson_6_Exceptions
    - script_1.py and script_2.py have been created
    - added samples on how to create a script that includes arguments

---
+++ src/script_1.py
@@ -0,0 +1,8 @@
+import sys
+
+argument_list = sys.argv
+print('The script has name', '', sys.argv[0])
+print('The number of arguments is', len(sys.argv))
+print('The Arguments are', argument_list)
+
+# python3 script_1.py 'lool' 'media' 12 //for run
